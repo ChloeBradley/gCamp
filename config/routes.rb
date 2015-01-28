eae1a77751565aef1bbf5ece75682761546c31a2
Chloe Bradley <ccbradley77@gmail.com>
2015-01-28 15:54:05 -0700

    added terms page, links

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
   root 'welcome#index'
-  
+  get 'terms' => 'terms#index'
+  get 'about' => 'about#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
