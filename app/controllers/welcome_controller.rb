12bca428ca405817b759afb6e8e07e4b85d8d95b
Chloe Bradley <ccbradley77@gmail.com>
2015-02-07 19:31:38 -0700

    adjusted quotes to be correct

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
 
   def index
-    @quote = [['"gCamp has changed my life! It\'s the best tool I\'ve ever used"','-Carla Hayes'],[
+    @quote= [['"gCamp has changed my life! It\'s the best tool I\'ve ever used"','-Carla Hayes'],[
     '"Before gCamp I was a disorderly slob. Now I\'m more organized than I\'ve ever been"','-Leta Jakolski'],['"Don\'t hesitate - sign up right now! You\'ll never be the same."', '-Lavern Upton']]
 end
 end
