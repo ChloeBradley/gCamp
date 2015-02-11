127db4256e67b1b0f4895e413f7995d1bef6fa9b
Chloe Bradley <ccbradley77@gmail.com>
2015-02-11 08:16:14 -0700

    worked through errors, still working on model to connect links

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,4 +7,20 @@ class TasksController < ApplicationController
   def show
     @tasks = Task.find(params[:id])
   end
+
+  def destroy
+    @tasks
+    def destroy
+      task= Task.find(params[:id])
+      task.destroy
+      redirect_to tasks_path
+    end
+  end
+
+
+  private
+
+  def tasks_params
+    params.require(:tasks).permit([])
+  end
 end
