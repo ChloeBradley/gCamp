e7f51f0fa15298e993039e2a70246d426ba61a8f
Chloe Bradley <ccbradley77@gmail.com>
2015-02-20 16:17:23 -0700

    added new form, created index page

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,52 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+    def destroy
+      user= User.find(params[:id])
+      user.destroy
+      redirect_to users_path
+    end
+
+    def create
+      @user = User.new(user_params)
+      if @user.save
+        flash[:notice] = "User was successfully created!"
+         redirect_to user_path(@user)
+      else
+        @user= User.new(user_params)
+        render:new
+      end
+    end
+
+    def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "User was successfully updated!"
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
+  end
+
+    def new
+      @user = User.new
+    end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email)
+  end
+end
