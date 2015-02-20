f7331120eec29a67b3f6a17d65ddf4e9b04dc300
Chloe Bradley <ccbradley77@gmail.com>
2015-02-19 18:01:04 -0700

    added users link and index view page

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@
   get '/faq'  => 'common_questions#index'
 
   resources :tasks
+  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
