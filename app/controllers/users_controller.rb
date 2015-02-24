727efead2d8995b81f9136d3520254fd6c039820
Chloe Bradley <ccbradley77@gmail.com>
2015-02-23 20:51:14 -0700

    user can delete users from edit page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,7 @@ class UsersController < ApplicationController
     def destroy
       user= User.find(params[:id])
       user.destroy
+      flash[:notice] = "User was successfully deleted"
       redirect_to users_path
     end
 
