1802ad32277090b24ce5dc228e76a9c8c15f3098
Chloe Bradley <ccbradley77@gmail.com>
2015-02-24 11:57:15 -0700

    added project link to footer/resources/projects controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@
 
   resources :tasks
   resources :users
+  resources :projects
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
