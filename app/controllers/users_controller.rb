85dc49b99bd12193f0fca26359889f10ea85bec9
Chloe Bradley <ccbradley77@gmail.com>
2015-02-23 20:28:16 -0700

    adjusted users paths to go to the right places

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,10 +21,9 @@ class UsersController < ApplicationController
     def create
       @user = User.new(user_params)
       if @user.save
-        flash[:notice] = "User was successfully created!"
+        flash[:success] = "User was successfully created!"
          redirect_to user_path(@user)
       else
-        @user= User.new(user_params)
         render:new
       end
     end
