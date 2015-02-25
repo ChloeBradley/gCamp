6f14e398face3e774d29dfc036c3c03cc3e33c3d
Chloe Bradley <ccbradley77@gmail.com>
2015-02-25 14:06:32 -0700

    Redirect user to user index after new user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,25 +12,25 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-    def destroy
-      user= User.find(params[:id])
-      user.destroy
-      flash[:notice] = "User was successfully deleted"
-      redirect_to users_path
-    end
+  def destroy
+    user= User.find(params[:id])
+    user.destroy
+    flash[:notice] = "User was successfully deleted"
+    redirect_to users_path
+  end
 
-    def create
-      @user = User.new(user_params)
-      if @user.save
-        flash[:success] = "User was successfully created!"
-         redirect_to user_path(@user)
-      else
-        render:new
-      end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "User was successfully created!"
+       redirect_to users_path
+    else
+      render:new
     end
+  end
 
-    def update
-    @user = User.find(params[:id])
+  def update
+  @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "User was successfully updated!"
       redirect_to users_path
@@ -39,9 +39,9 @@ class UsersController < ApplicationController
     end
   end
 
-    def new
-      @user = User.new
-    end
+  def new
+    @user = User.new
+  end
 
 
   private
