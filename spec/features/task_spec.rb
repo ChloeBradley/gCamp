6935cd98e1f7429772c964eb2f94c171e0e3174a
Chloe Bradley <ccbradley77@gmail.com>
2015-03-03 19:47:29 -0700

    4 passing Capybara specs (CRUD)

--- spec/features/task_spec.rb
+++ spec/features/task_spec.rb
@@ -1,8 +1,49 @@
 require 'rails_helper'
 
-feature "user can see gCamp on root page" do
-  it "gCamp is present" do
-    visit root_path
-    expect(page).to have_content "gCamp"
+feature "Task" do
+  scenario "user can see index page" do
+    task=Task.new(description:"string", due_date:"2015-03-12")
+    task.save!
+
+    visit tasks_path
+    expect(page).to have_content "string"
+    expect(page).to have_content "03/12/2015"
+  end
+
+  scenario "user can create new task" do
+    visit tasks_path
+    click_link "New Task"
+    expect(page).to have_content "New Task"
+
+    fill_in :task_description, with: "Anything!"
+    fill_in :task_due_date, with: '2015-04-12'
+    click_button "Create Task"
+
+    expect(page).to have_content "Task was successfully created!"
+    expect(page).to have_content "Anything!"
+    expect(page).to have_content "04/12/2015"
+  end
+
+  scenario "user can edit task" do
+    task=Task.new(description:"string", due_date:"2015-03-12")
+    task.save!
+    visit tasks_path
+    click_link "Edit"
+
+    expect(page).to have_content "Edit task"
+    fill_in :task_description, with: "More Things!"
+    fill_in :task_due_date, with: "2014-03-03"
+    click_button "Update Task"
+
+    expect(page).to have_content "Task was successfully updated!"
+    expect(page).to have_content "More Things!"
+    expect(page).to have_content "03/03/2014"
+  end
+
+  scenario "users can delete a task" do
+    task=Task.new(description:"string", due_date:"2015-03-12")
+    task.save!
+    visit tasks_path
+    click_link "Destroy"    
   end
 end
