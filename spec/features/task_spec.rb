86ca5e916104ff4c138877e00018d946cc70d55c
Chloe Bradley <ccbradley77@gmail.com>
2015-03-04 11:51:32 -0700

    created capybara feature specs and model spec

--- spec/features/task_spec.rb
+++ spec/features/task_spec.rb
@@ -44,6 +44,14 @@ feature "Task" do
     task=Task.new(description:"string", due_date:"2015-03-12")
     task.save!
     visit tasks_path
-    click_link "Destroy"    
+    click_link "Destroy"
+  end
+
+  scenario 'can see validations without a description' do
+    visit new_task_path
+    fill_in :task_due_date, with: "2015-03-12"
+    click_button "Create Task"
+    expect(page).to have_content '1 error prohibited this post from being saved:
+                                  Description can\'t be blank'
   end
 end
