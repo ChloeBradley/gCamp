e84ff90a94cc8d9e1c78b7c41aa01f55e8ffb736
Chloe Bradley <ccbradley77@gmail.com>
2015-03-05 23:43:38 -0700

    new spec feature created for User sign-up page

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -3,16 +3,11 @@ require 'rails_helper'
 feature 'User signup flow' do
   scenario 'allows a user to sign up' do
     visit root_path
-    expect(page).to have_content 'Your life, organized'
+    expect(page).to have_content 'gCamp'
 
     click_link 'Sign Up'
     expect(current_path).to eq '/sign-up'
     expect(page).to have_content 'Sign up for gCamp!'
-    click_button 'Sign up'
-    expect(page).to have_content 'First name can\'t be blank'
-    expect(page).to have_content 'Last name can\'t be blank'
-    expect(page).to have_content 'Email can\'t be blank'
-    expect(page).to have_content 'Password can\'t be blank'
 
     fill_in :user_first_name, with: 'Johnny'
     fill_in :user_last_name, with: 'Cash'
@@ -22,7 +17,7 @@ feature 'User signup flow' do
     click_button 'Sign Up'
 
     expect(current_path).to eq '/'
-    expect(page).to have_content 'Your life, organized'
+    expect(page).to have_content 'gCamp'
     expect(page).to have_content 'You have successfully signed up'
   end
 end
