a55a3745fa072a5cb25315f79051f9e44cecb648
Chloe Bradley <ccbradley77@gmail.com>
2015-03-06 00:36:55 -0700

    adjusted the order of validations in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
-  has_secure_password
 
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, uniqueness: true, presence: true
+
+  has_secure_password
 end
