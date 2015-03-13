3d8e56393bd176ede57816f2e88832554c6a85d2
Chloe Bradley <ccbradley77@gmail.com>
2015-03-13 14:09:19 -0600

    field with errors CSS added to the application css file to correct errors

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,8 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -22,5 +24,8 @@ module Gcamp
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance|
+  html_tag
+}
   end
 end
