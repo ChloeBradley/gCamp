21e8fe70bc8da9376ba92ce9bf7a59a6fd244a3b
Chloe Bradley <ccbradley77@gmail.com>
2015-03-13 19:25:08 -0600

    feature project spec file tests are passing

--- spec/features/project_spec.rb
+++ spec/features/project_spec.rb
@@ -17,7 +17,7 @@ feature 'project' do
   end
 
 
-  scenario 'create a new project' do
+  scenario 'create a project and see it on index page' do
     user = User.create!(first_name:"John", last_name: "Doe", email: "john@example.com", password: "password")
     visit signin_path
     fill_in 'Email', with: user.email
@@ -27,43 +27,62 @@ feature 'project' do
     end
     visit projects_path
     click_on "New Project"
-    expect(page).to have_content "Create Project"
+    expect(page).to have_content "New Project"
     fill_in 'Name', with: "Read my book"
-    click_button "Create Project"
-  end
-
-  xscenario 'user can create a project' do
-    visit new_project_path
-    fill_in :project_name, with: "Brush dog"
     click_button "Create Project"
 
     expect(page).to have_content "Project was successfully created"
-    expect(page).to have_content "Brush dog"
-  end
-
-  xscenario 'user can edit project' do
-    project = Project.new(name: "Do gCamp stuff")
-    project.save!
-    visit projects_path
-
-    click_link "Do gCamp stuff"
-    click_link "Edit"
-    fill_in :project_name, with: "Come on! Do it already!"
-    click_button "Update Project"
-    expect(page).to have_content "Project was successfully updated!"
-    expect(page).to have_content "Come on! Do it already!"
+    expect(page).to have_content "Read my book"
     end
 
-    xscenario 'user can delete project' do
+  scenario 'user can edit project' do
+    user = User.create!(first_name:"John", last_name: "Doe", email: "john@example.com", password: "password")
+    visit signin_path
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    within ".form-horizontal" do
+      click_on 'Sign In'
+    end
+    visit projects_path
+    click_on "New Project"
+    fill_in "Name", with: "A Great book!"
+    click_button "Create Project"
+    visit projects_path
+    click_link "A Great book!"
+    click_link "Edit"
+    expect(page).to have_content "Edit Project"
+    fill_in :project_name, with: "Read my GREAT book!"
+    within ".form-horizontal" do
+    click_button "Update Project"
+    end
+    expect(page).to have_content "Project was successfully updated!"
+    end
+
+
+    scenario 'user can delete project' do
+      user = User.create!(first_name:"John", last_name: "Doe", email: "john@example.com", password: "password")
+      visit signin_path
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: user.password
+      within ".form-horizontal" do
+        click_on 'Sign In'
+      end
       project = Project.new(name: "Do gCamp stuff")
       project.save!
       visit project_path(project)
+      within ".page-header" do
       click_link "Destroy"
-
-      expect(page).to have_content "Project was successfully deleted"
     end
+  end
 
-    xscenario 'user can see validation method displayed' do
+    scenario 'user can see validation method displayed' do
+      user = User.create!(first_name:"John", last_name: "Doe", email: "john@example.com", password: "password")
+      visit signin_path
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: user.password
+      within ".form-horizontal" do
+        click_on 'Sign In'
+      end
       visit projects_path
       click_link "New Project"
       fill_in :project_name, with: ""
