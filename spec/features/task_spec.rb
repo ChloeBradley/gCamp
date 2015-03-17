20881defded34916aed558fc330ea0989554c264
Chloe Bradley <ccbradley77@gmail.com>
2015-03-16 18:05:13 -0600

    working on running capybara specs

--- spec/features/task_spec.rb
+++ spec/features/task_spec.rb
@@ -2,29 +2,33 @@ require 'rails_helper'
 
 feature "Task" do
   scenario "user can see index page" do
-    task=Task.new(description:"string", due_date:"2015-03-12")
+    task=Task.new(description:"Be a happy camper", due_date:"2015-03-12")
     task.save!
-
-    visit tasks_path
-    expect(page).to have_content "string"
-    expect(page).to have_content "03/12/2015"
+    user = User.create!(first_name:"John", last_name: "Doe", email: "john@example.com", password: "password")
+    visit signin_path
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    within ".form-horizontal" do
+      click_on 'Sign In'
+    end
+    visit projects_path
+    expect(page).to have_content "Projects"
   end
 
   scenario "user can create new task" do
-    visit tasks_path
-    click_link "New Task"
-    expect(page).to have_content "New Task"
+    binding.pry
+    project = Project.new.create!(name:"Giant project")
+    project.save!
+      user = User.create!(first_name:"John", last_name: "Doe", email: "john@example.com", password: "password")
+      visit signin_path
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: user.password
+      within ".form-horizontal" do
+        click_on 'Sign In'
+      end
+    end
 
-    fill_in :task_description, with: "Anything!"
-    fill_in :task_due_date, with: '2015-04-12'
-    click_button "Create Task"
-
-    expect(page).to have_content "Task was successfully created!"
-    expect(page).to have_content "Anything!"
-    expect(page).to have_content "04/12/2015"
-  end
-
-  scenario "user can edit task" do
+  xscenario "user can edit task" do
     task=Task.new(description:"string", due_date:"2015-03-12")
     task.save!
     visit tasks_path
@@ -40,14 +44,14 @@ feature "Task" do
     expect(page).to have_content "03/03/2014"
   end
 
-  scenario "users can delete a task" do
+  xscenario "users can delete a task" do
     task=Task.new(description:"string", due_date:"2015-03-12")
     task.save!
     visit tasks_path
     click_link "Destroy"
   end
 
-  scenario 'can see validations without a description' do
+  xscenario 'can see validations without a description' do
     visit new_task_path
     fill_in :task_due_date, with: "2015-03-12"
     click_button "Create Task"
