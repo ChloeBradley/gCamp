56e315a723765f1885a6c3646753f9a3788c09ca
Chloe Bradley <ccbradley77@gmail.com>
2015-03-18 16:53:34 -0600

    project show page confirms tasks(s) and membership(s) will be deleted

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,9 @@
 class Task < ActiveRecord::Base
+
+  validates :due_date, presence: true
   validates :description, presence: true
   belongs_to :project
+
 
   has_many :comments
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,7 @@ class User < ActiveRecord::Base
 
   has_many :memberships
   has_many :tasks, through: :projects
+  has_many :comments, through: :tasks
 
   has_secure_password
 
