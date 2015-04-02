b4ff9ae8e91ec0f30e1811da90ad248307ca026b
Chloe Bradley <ccbradley77@gmail.com>
2015-04-01 19:43:11 -0600

    commiting pivotal tracker commits

---
+++ app/controllers/pivotaltracker_controller.rb
@@ -0,0 +1,7 @@
+class PivotaltrackerController < ApplicationController
+  def show
+    tracker_api = TrackerAPI.new
+    proj_id = (params[:id])
+    @stories = tracker_api.stories(current_user.tracker_token, proj_id)
+  end
+end
