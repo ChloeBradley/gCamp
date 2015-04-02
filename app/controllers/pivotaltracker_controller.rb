acec74feb6aad38c8a13367a902ccf00ad787ec0
Chloe Bradley <ccbradley77@gmail.com>
2015-04-02 14:30:18 -0600

    user can see table of stories for a pivotal tracker project

--- app/controllers/pivotaltracker_controller.rb
+++ app/controllers/pivotaltracker_controller.rb
@@ -1,5 +1,6 @@
 class PivotaltrackerController < ApplicationController
   def show
+    @project_name = params["name"]
     tracker_api = TrackerAPI.new
     proj_id = (params[:id])
     @stories = tracker_api.stories(current_user.tracker_token, proj_id)
